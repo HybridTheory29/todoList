2571bdf9035d12c70d398ab5e6c5775fd6dafe99
HybridTheory29 <kirdyashkin.kiroman@gmail.com>
2025-04-25 22:24:04 +0300

    appp

--- main/signals.py
+++ main/signals.py
@@ -1,10 +1,11 @@
+"""
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 from .models import Task
 from .utils import notify_bot
 
-"""
+
 @receiver(post_save, sender=Task)
 def check_task_overdue(sender, instance, created, **kwargs):
     if (
